767284650f9f4685e15d2c6d8651f929a511ca8a
TAKUYA-HIRANO25 <k023g0013@g.neec.ac.jp>
2025-05-29 11:08:28 +0900

    途中

--- DirectXGame/Effect.cpp
+++ DirectXGame/Effect.cpp
@@ -2,14 +2,22 @@
 using namespace KamataEngine;
 using namespace MathUtility;
 
-void Effect::Initialize(Model* model) {
+void Effect::Initialize(Model* model,Vector3 scale, Vector3 rotate) {
 	assert(model);
 
 	model_ = model;
 
+	scale_ = scale;
+	rotation_ = rotate;
+
 	worldTransform_.Initialize();
 }
 
-void Effect::Update() { worldTransform_.TransferMatrix(); }
+void Effect::Update() { 
+	worldTransform_.scale_ = scale_;
+	worldTransform_.rotation_ = rotation_;
+
+	worldTransform_.UpdateMatrix();
+}
 
 void Effect::Drow(Camera& camera) { model_->Draw(worldTransform_, camera); }
--- DirectXGame/Effect.h
+++ DirectXGame/Effect.h
@@ -3,7 +3,7 @@
 
 class Effect {
 public:
-	void Initialize(KamataEngine::Model* model);
+	void Initialize(KamataEngine::Model* model, KamataEngine::Vector3 scale, KamataEngine::Vector3 rotate);
 
 	void Update();
 
@@ -13,5 +13,8 @@ private:
 	KamataEngine::WorldTransform worldTransform_;
 
 	KamataEngine::Model* model_ = nullptr;
+
+	KamataEngine::Vector3 scale_;
+	KamataEngine::Vector3 rotation_;
 };
 
